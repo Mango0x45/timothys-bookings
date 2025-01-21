9b726615b400f37e02696a0197d471a470f2aaef
Ibrahim Iqbal <ibrahim@ebs.net.pk>
2025-01-21 17:52:09 +0100

    hello world

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
 
 func main() {
+
 	fmt.Println("Hello, World")
+	http.HandleFunc("/hello", getHello)
+	err := http.ListenAndServe(":6969", nil)
+	fmt.Print(err)
+
+}
+
+func getHello(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Printf("Got /hello request\n")
+	io.WriteString(w, "Hello, HTTP!\n")
 }
